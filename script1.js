a90ebf39d641e880320f27aa075b0c4502f7ba2a
Och11nko <157588989+Och11nko@users.noreply.github.com>
2024-03-19 19:47:15 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,49 @@
+var score = 0
+
+if (answer1 == 1995) {
+    score = +1
+}
+
+if (answer1 == Brendan Eich) {
+    score = +1
+}
+
+if (answer1 == var) {
+    score = +1
+}
+
+if (answer1 == Angles) {
+    score = +1
+}
+
+if (answer1 == Add interactivity to the website) {
+    score = +1
+}
+
+if (answer1 == Clicks the object using the mouse) {
+    score = +1
+}
+
+if (answer1 == Can access web pages from another domain) {
+    score = +1
+}
+
+if (answer1 == False) {
+    score = +1
+}
+
+if (answer1 == Yes) {
+    score = +1
+}
+
+if (answer1 == To make pages interactive) {
+    score = +1
+}
+
+if (answer1 == Both A and B) {
+    score = +1
+}
+
+if (answer1 == All of the Above) {
+    score = +1
+}
